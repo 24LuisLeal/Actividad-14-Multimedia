d55d8a7b5510d38ed796805eedd265ea143c827b
LuisLeal24 <61062727+LuisLeal24@users.noreply.github.com>
2021-11-18 22:20:34 -0600

    Interacción con el usuario

--- PartOne/booleans.js
+++ PartOne/booleans.js
@@ -1,15 +1,22 @@
-var myService = true;
-//intenta colocando 1 ó 0 en lugar de true / false
-if (myService) {
-  var siSer = document.getElementById('servicio');
-  siSer.className = 'st';
-} else {
-  var noSer = document.getElementById('servicio');
-  noSer.className = 'sh';
-}
 
 
-function lightOne() {
-    var background = document.getElementById("one");
-    background.style.backgroundColor = "red";
+
+
+
+
+
+
+
+
+
+function setStatus(status){
+  var myService = status;
+  var text = document.getElementById('servicio');
+  
+  if (myService) {
+    text.className = 'available';
+  
+  } else {  
+    text.className = 'disabled';
+  }
 }
